10f1b00755c499531932524e695deb5fbf6a75ff
charitoitu-commits <charitoitu@icloud.com>
2026-04-28 20:30:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function irMenu() {
+  document.getElementById("menu").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function reservar() {
+  alert("Gracias por reservar en Clorindo Café");
+}
+
+function comoLlegar() {
+  alert("Estamos en Buenos Aires. Próximamente mapa disponible.");
+}
